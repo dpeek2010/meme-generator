7e1c2055a217ad7e9f78d1d8c0acc5d9a15b1221
dpeek2010 <dpeek2010@yahoo.com>
2023-12-07 13:53:19 -0600

    optmimized setMeme

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,13 +2,27 @@ import memeData from "../memeData";
 import React from "react";
 
 function Meme() {
-  const [memeImage, setMemeImage] = React.useState("");
+  /* const [memeImage, setMemeImage] = React.useState(
+    "https://i.imgflip.com/1ur9b0.jpg"
+  ); */
+
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/1ur9b0.jpg",
+  });
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memeData);
 
   function getRandomMeme() {
-    const memesArray = memeData.data.memes;
+    const memesArray = allMemeImages.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    setMemeImage(memesArray[randomNumber].url);
-    // const url = memesArray[randomNumber].url;
+    //setMemeImage(memesArray[randomNumber].url);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
   }
 
   return (
@@ -20,7 +34,7 @@ function Meme() {
           Get a new meme image 🖼
         </button>
       </div>
-      <img src={memeImage} classname="meme--image" alt="meme" />
+      <img src={meme.randomImage} className="meme--image" alt="meme" />
     </main>
   );
 }
